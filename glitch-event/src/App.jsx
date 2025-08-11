3021887a1433dff095f59c12ae332025cfac46bc
Parikshit <parikshit.r2005@gmail.com>
2025-08-11 22:21:56 +0530

    initial commit

---
+++ glitch-event/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  return(
+    <div>
+      <span className='text-2xl font-bold underline'>
+        Hello Vite + React!
+      </span>
+    </div>
+  )
+  
+}
+
+export default App
