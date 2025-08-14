cd3db639b744b0bdb4bd9b298d92020e98111b8f
KK-8848 <karthikkrishna8848@gmail.com>
2025-08-14 22:19:12 +0530

    update:added easter egg

--- glitch-event/src/components/sections/EventRegistrationHeader.jsx
+++ glitch-event/src/components/sections/EventRegistrationHeader.jsx
@@ -6,13 +6,19 @@ const EventRegistrationHeader = () => {
     <section className="bg-[#0D0E20] py-16 md:py-20">
       <div className="container mx-auto px-4 text-center">
         <div className="mb-10 flex justify-center">
-          <img
-            src={logo}
-            alt="Glitch Logo"
-            width={238}
-            height={62}
-            className="object-contain"
-          />
+          <div className="relative group">
+            <img
+              src={logo}
+              alt="Glitch Logo"
+              width={238}
+              height={62}
+              className="object-contain cursor-pointer"
+            />
+          
+            <div className="absolute -bottom-0 right-0 bg-gray-800/80 text-gray-400 text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none hidden md:block">
+              Made by Leon Tom 
+            </div>
+          </div>
         </div>
 
         <div className="max-w-2xl mx-auto mb-6">
